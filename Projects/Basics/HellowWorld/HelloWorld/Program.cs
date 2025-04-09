7ac509b7903436a509a08acc357e2a99ff770de2
babyboydaprince <balabenutemateus@hotmail.com>
2025-04-08 23:51:34 -0300

    :hammer: Studying

---
+++ Projects/Basics/HellowWorld/HelloWorld/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World! C#");
+            // Console.WriteLine(5 + 5);
+
+            // int varPrinter = 15;
+            // Console.WriteLine(varPrinter);
+
+            // string varPrinter = "String type print";
+            // Console.WriteLine(varPrinter);
+
+            // const string varPrinter = "Testing a constant";
+            // Console.WriteLine(varPrinter);
+
+            // string varPrinter = "Your number is: ";
+            // int varNumber = 5;
+            // Console.WriteLine(varPrinter + varNumber);
+            // Console.WriteLine("Your number is: " + varNumber);
+
+            // int x = 10;
+            // int y = 5;
+            // Console.WriteLine(x + y);
+
+            // int x = 5, y = 10;
+            // Console.WriteLine(x + y);
+
+            // bool varPrinter = true;
+            // Console.WriteLine(varPrinter);
+
+            // double myDouble = 5.5;
+            // int myInt = (int)myDouble;
+            // Console.WriteLine($"The data type of myInt is: {myInt.GetType().Name}");
+            // Console.WriteLine($"The Full data type of myInt is: {myInt.GetType().FullName}");
+
+            // double myDouble = 5.5;
+            // int myInt = (int)myDouble;
+            // Console.WriteLine($"The data type of myInt is: {myInt.GetType().Name}");
+            // Console.WriteLine($"The Full data type of myInt is: {myInt.GetType().FullName}");
+            // Console.WriteLine($"Converting the integer value to string: {Convert.ToString(myInt).GetType().Name}");
+
+            // Console.WriteLine($"Place a string input: ");
+            // string myInput = Console.ReadLine();
+            // Console.WriteLine($"You entered: {myInput}");
+
+            // OPERATORS
+            /*
+             * == Comparison
+             * != different
+             * && OR
+             * || xOR
+             * ! NOT
+             * >= Greater equals
+             * <= Lesser equals
+             * + SUM
+             * - MINUS
+             * 8 MULTIPLY
+             * '/' DEVIDE
+             * ++ INCREMENTS 1
+             * -- DECREMENTS 1
+             */
+        }
+    }
+}
