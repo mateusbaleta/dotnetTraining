44406c9e2c9c64e4eb27724b1e679b88fc565a52
babyboydaprince <balabenutemateus@hotmail.com>
2025-04-10 23:56:20 -0300

    :hammer: Studying

--- Projects/Basics/HellowWorld/HelloWorld/Program.cs
+++ Projects/Basics/HellowWorld/HelloWorld/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Sharp as Tramontina! \n\n");
+
             // Console.WriteLine("Hello World! C#");
             // Console.WriteLine(5 + 5);
 
@@ -48,20 +50,82 @@
 
             // OPERATORS
             /*
-             * == Comparison
-             * != different
-             * && OR
-             * || xOR
-             * ! NOT
-             * >= Greater equals
-             * <= Lesser equals
-             * + SUM
-             * - MINUS
-             * 8 MULTIPLY
-             * '/' DEVIDE
-             * ++ INCREMENTS 1
-             * -- DECREMENTS 1
-             */
+            * == Comparison
+            * != different
+            * && OR
+            * || xOR
+            * ! NOT
+            * >= Greater equals
+            * <= Lesser equals
+            * + SUM
+            * - MINUS
+            * 8 MULTIPLY
+            * '/' DEVIDE
+            * ++ INCREMENTS 1
+            * -- DECREMENTS 1
+            */
+
+            // Grabs the Minimum value between two numbers
+		    // double result = Math.Min(5, 10);
+		    // Console.WriteLine(result);
+
+            // Grabs the Maximum value between two numbers
+		    // double result = Math.Max(5, 10);
+		    // Console.WriteLine(result);
+
+            // Grabs the square root of a value
+		    // double result = Math.Sqrt(10);
+		    // Console.WriteLine(result);
+
+
+            // Grabs the round result of a value
+		    // double result = Math.Round(2.6);
+		    // Console.WriteLine(result);
+
+            // Grabs the absolute number of a value
+		    // double result = Math.Abs(-4.7);
+		    // Console.WriteLine(result);
+
+            // Convert to uppercase
+			// string result = "Uppercut!";
+		    // Console.WriteLine(result.ToUpper());
+
+            // Lenght of a value
+	        // string result = "Uppercut!";
+		    // Console.WriteLine(result.Length);
+
+            // Convert to lowercase
+            // string result = "SHRINK!";
+		    // Console.WriteLine(result.ToLower());
+
+            // Concatenate strings
+	        // string firstName = "Sometimes the 1st, ";
+	        // string secondName = "Others the last, \n";
+	        // string thirdName = "But you won't get me out of the Race!";
+		    // Console.WriteLine(string.Concat(firstName, secondName, thirdName));
+
+            // Concatenate strings
+			// string firstName = "Sometimes the 1st, ";
+			// string secondName = "Others the last, \n";
+			// string thirdName = "But you won't get me out of the Race!";
+		    // Console.WriteLine(string.Concat(firstName, secondName, thirdName));
+
+			// Slicing
+			// string firstName = "Reach the 1st";
+			// Console.WriteLine(firstName[0]);
+
+			// Get index
+			// string firstName = "Indexing...";
+			// Console.WriteLine(firstName.IndexOf("x"));
+
+			// usage of substring to grab the word that belongs to a specific character and ahead of it
+			// initially grabbed by index
+			string name = "Grab the Third Word";
+			int charPos = name.IndexOf("T");
+			string thirdWord = name.Substring(charPos);
+			Console.WriteLine(thirdWord);
+
+
         }
     }
 }
